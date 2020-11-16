355a9a5b5d111cdf449363f2a68a0a7009f55d7b
Shion <ehdrhelr@gmail.com>
2020-11-16 16:23:12 +0900

    Add command 'cd' in help

--- src/day11/shell/Help.java
+++ src/day11/shell/Help.java
@@ -9,6 +9,7 @@ public class Help extends Thread {
         System.out.println(" history       show the last 10 commands");
         System.out.println(" cp            copy the file and paste in the directory");
         System.out.println(" mkdir         make the directory");
+        System.out.println(" cd            change the current directory");
         System.out.println(" exit          exit the process");
     }
 }
