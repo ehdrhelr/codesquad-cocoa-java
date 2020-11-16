4e358a58aa6c940c35045067107e76c909fdd6cf
Shion <ehdrhelr@gmail.com>
2020-11-16 13:22:22 +0900

    Add command 'history'

--- src/day11/shell/Shell.java
+++ src/day11/shell/Shell.java
@@ -14,15 +14,21 @@ public class Shell {
         while (true) {
             System.out.print("Java Shell> ");
             String command = sc.nextLine();
-            if (command.equalsIgnoreCase("ls")) {
+            History.saveInHistory(command);
+            if (command.equalsIgnoreCase("ls")) { // Runnable 구현
                 Runnable list = new List();
                 Thread listThread = new Thread(list);
                 listThread.start();
                 Thread.sleep(10);
             }
-            if (command.equalsIgnoreCase("pwd")) {
+            if (command.equalsIgnoreCase("pwd")) { // Runnalbe 구현
                 Thread directoryThread = new Thread(new Directory());
                 directoryThread.start();
+                Thread.sleep(10);
+            }
+            if (command.equalsIgnoreCase("history")) { // Thread 상속
+                History historyThread = new History();
+                historyThread.start();
                 Thread.sleep(10);
             }
         }
