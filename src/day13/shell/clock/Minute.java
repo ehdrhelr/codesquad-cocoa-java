56903d9b0222ae53c2a72c42288b522ef32cd241
Shion <ehdrhelr@gmail.com>
2020-11-18 16:49:33 +0900

    Add diplay minute over ten

--- src/day13/shell/clock/Minute.java
+++ src/day13/shell/clock/Minute.java
@@ -10,7 +10,17 @@ public class Minute {
         minuteOverTen = minuteBoard.getMinuteBoardOverTen();
 
         for (int i = 1; i < minuteOverTen.length; i++) {
-            System.out.print(minuteOverTen[i] + " ");
+            int firstNumber = getFirstNumberOfMinute(time[1]);
+            if (i == minuteOverTen.length - 1) { // '십'을 출력할 때 10분 이상일 경우 '십'에 불켜준다.
+                lightSibOn(i, time[1]);
+                continue;
+            }
+            if (time[1] >= 20 && i == firstNumber) {
+                lightFirstNumberOfMinuteOn(firstNumber);
+                continue;
+            }
+            System.out.print(FontColor.ANSI_RESET.getValue() + minuteOverTen[i] + " ");
+
         }
         System.out.println();
     }
@@ -20,7 +30,8 @@ public class Minute {
         minuteUnderTen = minuteBoard.getMinuteboardUnderTen();
         for (int i = 0; i < minuteUnderTen.length; i++) {
             moveNextLineWhenTargetIndex(i);
-            System.out.print(minuteUnderTen[i] + " ");
+
+            System.out.print(FontColor.ANSI_RESET.getValue() + minuteUnderTen[i] + " ");
         }
     }
 
@@ -28,5 +39,19 @@ public class Minute {
         if (i == 6) {
             System.out.println();
         }
+    }
+
+    public void lightSibOn(int i, int minute) {
+        if (minute >= 10) {
+            System.out.print(FontColor.ANSI_CYAN.getValue() + minuteOverTen[i] + " ");
+        }
+    }
+
+    public int getFirstNumberOfMinute(int minute) { // 20 이상일 때만 실행한다.
+        return minute / 10;
+    }
+
+    public void lightFirstNumberOfMinuteOn(int firstNumberOfMinute) {
+        System.out.print(FontColor.ANSI_CYAN.getValue() + minuteOverTen[firstNumberOfMinute] + " ");
     }
 }
