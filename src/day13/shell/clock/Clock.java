f75fcac52b9082f22df71d2b66cdb3fc0ed8e1b4
Shion <ehdrhelr@gmail.com>
2020-11-18 15:01:21 +0900

    Fix display hour 11, 12, noon, midnight

--- src/day13/shell/clock/Clock.java
+++ src/day13/shell/clock/Clock.java
@@ -4,8 +4,10 @@ import java.util.Calendar;
 
 public class Clock {
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_CYAN = "\u001B[36m";
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+
+    private String[] hourOnClock;
 
     public static void main(String[] args) {
         Clock clock = new Clock();
@@ -17,14 +19,14 @@ public class Clock {
         Calendar today = Calendar.getInstance();
         int hour = today.get((Calendar.HOUR));
         int minute = today.get(Calendar.MINUTE);
-        int second = today.get(Calendar.SECOND);
+        int[] time = {0, minute};
 
-        display(hour);
+        display(time);
 
     }
 
-    public void display(int hour) {
-        displayHour(hour);
+    public void display(int[] time) {
+        displayHour(time);
     }
 
     public void moveNextLineWhenTargetHourIndex(int i) {
@@ -32,20 +34,54 @@ public class Clock {
         if (i == 9) System.out.println();
     }
 
-    public void displayHour(int hour) {
+    public void displayHour(int[] time) {
         HourBoard hourBoard = new HourBoard();
-        String[] hourOnClock = hourBoard.getHourBoard();
+        hourOnClock = hourBoard.getHourBoard();
         for (int i = 1; i < hourBoard.getHourBoard().length; i++) {
-            if (i == hour) {
-                System.out.print(ANSI_CYAN + hourOnClock[i] + " " + ANSI_RESET);
+            if (i == hourOnClock.length - 1) { // '시'에 도착했을 때 자정인지 아닌지 체크해서 불켜준다.
+                lightSiOn(i, time);
                 continue;
             }
-            if (i == hourBoard.getHourBoard().length - 1) {
-                System.out.print(ANSI_CYAN + hourOnClock[i] + " " + ANSI_RESET);
+            if (time[0] >= 10 && i == 10) { // 열시일 때 '열'에 불켜준다.
+                lightTenOn();
                 continue;
             }
-            moveNextLineWhenTargetHourIndex(i);
-            System.out.print(hourOnClock[i] + " ");
+            if (i == time[0] && i >= 10) { // 열시 이상일 때 '열' 불켜진 상태로 '한' 또는 '두'에 불켜준다.
+                lightTargetHourOverTenOn(i);
+                continue;
+            }
+            if (i <= 10 && i == time[0]) { // 열시 미만일 때 불켜준다.
+                lightTargetHourOn(i);
+                continue;
+            }
+
+            moveNextLineWhenTargetHourIndex(i); // 화면 이쁘게 줄바꿔준다.
+            printOtherHours(i); // 현재 시간이외의 문자들을 플레인으로 출력
         }
     }
+
+    public void lightTargetHourOn(int i) {
+        System.out.print(ANSI_CYAN + hourOnClock[i] + " ");
+    }
+
+    public void lightTargetHourOverTenOn(int i) {
+        System.out.print(ANSI_CYAN + hourOnClock[i] + " ");
+    }
+
+    public void lightTenOn() {
+        System.out.print(ANSI_CYAN + hourOnClock[10] + " ");
+    }
+
+    public void lightSiOn(int i, int[] time) {
+        if (time[0] == 0 || time[0] == 12) { // 자정이거나 정오일 때 '시' 표시 안한다.
+            System.out.print(ANSI_RESET + hourOnClock[i] + " ");
+            return;
+        }
+        System.out.print(ANSI_CYAN + hourOnClock[i] + " ");
+    }
+
+    public void printOtherHours(int i) {
+        System.out.print(ANSI_RESET + hourOnClock[i] + " ");
+    }
+
 }
---
+++ test/ClockTest.java
@@ -0,0 +1,15 @@
+import day13.shell.clock.Clock;
+import org.junit.Test;
+
+public class ClockTest {
+    Clock c = new Clock();
+
+    @Test
+    public void testDisplay() {
+        int[] time = {12, 0};
+        c.display(time);
+
+
+    }
+
+}
