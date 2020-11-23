0b478ed84ac63430743d99b1c96ffd8c1cb5739b
Shion <ehdrhelr@gmail.com>
2020-11-23 17:13:55 +0900

    Impl intro background

--- src/day16/dynamic_beat/Main.java
+++ src/day16/dynamic_beat/Main.java
@@ -1,8 +1,8 @@
 package day16.dynamic_beat;
 
 public class Main {
-    public static final int SCREEN_WIDTH = 720;
-    public static final int SCREEN_HEIGHT = 480;
+    public static final int SCREEN_WIDTH = 960;
+    public static final int SCREEN_HEIGHT = 600;
 
     public static void main(String[] args) {
         new DynamicBeat();
