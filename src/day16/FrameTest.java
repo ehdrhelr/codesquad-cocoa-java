e3c4a73d243ef2fbfed8c61388dc79d176e50c7e
Shion <ehdrhelr@gmail.com>
2020-11-23 11:14:40 +0900

    Init gui programming practice

---
+++ src/day16/FrameTest.java
@@ -0,0 +1,11 @@
+package day16;
+
+import java.awt.*;
+
+public class FrameTest {
+    public static void main(String[] args) {
+        Frame f =  new Frame("Login");
+        f.setSize(300, 200);
+        f.setVisible(true);
+    }
+}
