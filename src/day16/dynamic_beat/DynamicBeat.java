e72ad261dc8916a7361cc6bb75ca1badaaa31f27
Shion <ehdrhelr@gmail.com>
2020-11-23 17:54:54 +0900

    Add intro music

--- src/day16/dynamic_beat/DynamicBeat.java
+++ src/day16/dynamic_beat/DynamicBeat.java
@@ -19,6 +19,9 @@ public class DynamicBeat extends JFrame {
         setVisible(true);
 
         introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
+
+        Music introMusic = new Music("introMusic.mp3", true);
+        introMusic.start();
     }
 
     public void paint(Graphics g) {
